623c542c41ae45edceef296c0aea9c10e379ff12
Jackob <saxolita@ya.ru>
2019-06-13 19:27:07 +1000

    Add FileLoader class for service.

--- src/Libr/CRUDBundle/Service/FileLoader.php
+++ src/Libr/CRUDBundle/Service/FileLoader.php
@@ -6,5 +6,13 @@ namespace Libr\CRUDBundle\Service;
 
 class FileLoader
 {
+    public function saveFile($src, $dist){
+        try{
+            move_uploaded_file($src, $dist);
+        } catch (\Exception $e){
+            throw $e;
+        }
+        return 1;
+    }
 
 }
