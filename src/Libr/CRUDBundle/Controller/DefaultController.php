a92e9f8bde5be8d2ebd20e45a6c0ef25870850dd
Jackob <saxolita@ya.ru>
2019-06-15 20:35:36 +1000

    Change absolute paths to kernel.root_dir.

--- src/Libr/CRUDBundle/Controller/DefaultController.php
+++ src/Libr/CRUDBundle/Controller/DefaultController.php
@@ -2,7 +2,7 @@
 
 
 namespace Libr\CRUDBundle\Controller;
-require_once '/home/jackob/Desktop/MaProj/Test-task-for-Lab-Fakt/CRUD_for_lib/vendor/fzaninotto/faker/src/autoload.php';
+require __DIR__.'/../../../../vendor/fzaninotto/faker/src/autoload.php';
 
 use Fakerino\Fakerino;
 use Libr\CRUDBundle\Entity\Authors;
@@ -24,33 +24,35 @@ class DefaultController extends Controller
     }
 
     /**
-     * @Route("/generateFakeData")
+     * @Route("/generateFakeData/{recordCount}")
      * @Method("GET")
      */
-    public function generateFakeDataAction()
+    public function generateFakeDataAction($recordCount)
     {
+        if($recordCount == 0){
+            return new Response('Nothing to create');
+        }
         $faker = Factory::create();
-        $count_of_records = 0;
         $em = $this->getDoctrine()->getManager();
-        $img_path = '/home/jackob/Desktop/MaProj/Test-task-for-Lab-Fakt/CRUD_for_lib/web/uploaded_imgs';
-        for ($i = 0; $i < $count_of_records; $i++) {
+        $img_path = $this->get('kernel')->getRootDir().'/../web/uploaded_imgs';
+        for ($i = 0; $i < $recordCount; $i++) {
             /*
              * Generate Books
              * */
             $book = new Books();
-            $book->setBookPubDate($faker->dateTimeBetween($startDate = '-30 years', $endDate = 'now'));
-            $book->setBookDesc($faker->text());
-            $book->setBookName($faker->name());
+            $book->setPublicationDate($faker->dateTimeBetween($startDate = '-30 years', $endDate = 'now'));
+            $book->setDescription($faker->text());
+            $book->setTitle($faker->name());
             $splited_path = preg_split("(/)", $faker->image($img_path, 640, 480));
-            $book->setBookImg('/uploaded_imgs/' . $splited_path[sizeof($splited_path) - 1]);
+            $book->setImgPath('/uploaded_imgs/' . $splited_path[sizeof($splited_path) - 1]);
             /*******************************************/
 
             /*
              * Generate Authors
              * */
             $author = new Authors();
-            $author->setAuthorName($faker->firstName);
-            $author->setAuthorSecondName($faker->lastName);
+            $author->setFirstName($faker->firstName);
+            $author->setSecondName($faker->lastName);
 
             $em->persist($book);
             $em->persist($author);
@@ -61,10 +63,11 @@ class DefaultController extends Controller
         $authors = $em->getRepository('LibrCRUDBundle:Authors')->findAll();
         $authors_size = sizeof($authors);
         foreach ($books as &$book) {
-            if(sizeof($book->getAuthor()) > 0)
+            if (sizeof($book->getAuthor()) > 0) {
                 continue;
+            }
             $book->addAuthor($authors[mt_rand(0, (int)($authors_size / 2))]);
-            $book->addAuthor($authors[mt_rand(((int)($authors_size / 2))+1, $authors_size-1)]);
+            $book->addAuthor($authors[mt_rand(((int)($authors_size / 2)) + 1, $authors_size - 1)]);
 
         }
         $em->flush();
